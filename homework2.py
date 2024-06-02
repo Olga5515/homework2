ea56b7f6accb02bcae4746416125565f6a2e3a28
Olga5515 <torfenita@mail.ru>
2024-06-02 12:55:58 +1000

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+course_name = 'Python'
+completed_homework = 12
+hours_spent = 1.5
+print('Курс:', course_name, '; всего задач:', completed_homework, '; затрачено часов:', hours_spent, '; среднее время выполнения:', hours_spent / completed_homework, 'часа.')
+  # отправлено 2024/06/02 12:55 по теме "Переменные"
