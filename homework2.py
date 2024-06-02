78fe2386313adba8b0d242120dad2a10b200bc3b
Olga5515 <torfenita@mail.ru>
2024-06-02 21:34:56 +1000

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -1,5 +1,6 @@
 course_name = 'Python'
 completed_homework = 12
 hours_spent = 1.5
-print('Курс:', course_name, '; всего задач:', completed_homework, '; затрачено часов:', hours_spent, '; среднее время выполнения:', hours_spent / completed_homework, 'часа.')
-  # отправлено 2024/06/02 12:55 по теме "Переменные"
+average_value = hours_spent / completed_homework
+print('Курс:', course_name, '; всего задач:', completed_homework, '; затрачено часов:', hours_spent, '; среднее время выполнения:', average_value, 'часа.')
+# исправлено по замечанию преподавателя: внесена четвертая переменная; отправлено 2024/06/02 21:30
